c07bc0899ef70f8a5af3895b4887265c80574525
Samin <Samin@DESKTOP-B8FQBKL>
2020-08-17 22:58:00 +0430

    first commit

---
+++ src/Fixer.Tracing.Jaeger.RabbitMQ/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Fixer.MessageBrokers.RabbitMQ;
+using Fixer.Tracing.Jaeger.RabbitMQ.Middlewares;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fixer.Tracing.Jaeger.RabbitMQ
+{
+    public static class Extensions
+    {
+
+        public static IFixerBuilder AddJaegerRabbitMqMiddleware(this IFixerBuilder builder)
+        {
+            builder.Services.AddTransient<IRabbitMqMiddleware, JaegerMiddleware>();
+
+            return builder;
+        }
+    }
+}
